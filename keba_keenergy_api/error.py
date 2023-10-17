dbaa2f30b67dadfd37ba1583f1826e68f68614e3
Michael Hacker <mh@superbox.one>
2023-10-17 20:52:44 +0200

    `InvalidJsonError` Class no inherit from `APIError` Class.

--- keba_keenergy_api/error.py
+++ keba_keenergy_api/error.py
@@ -1,9 +1,9 @@
 """KEBA KeEnergy API error classes."""
 
 
-class InvalidJsonError(Exception):
-    """Invalid JSON Data Error."""
-
-
 class APIError(Exception):
     """API Error."""
+
+
+class InvalidJsonError(APIError):
+    """Invalid JSON Data Error."""
